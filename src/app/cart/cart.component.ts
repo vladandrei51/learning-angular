6dbbdeed8ea400d7e61e29cabbcb0e735c5dc12d
vladandrei51 <vladandrei51@gmail.com>
2020-01-21 11:07:51 +0200

    added forms for checkout

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { CartService } from '../cart.service';
+import { Component, OnInit } from "@angular/core";
+import { CartService } from "../cart.service";
+import { FormBuilder } from "@angular/forms";
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+  styleUrls: ["./cart.component.css"]
 })
 export class CartComponent implements OnInit {
-
   items;
-
+  checkoutForm;
   total;
 
-  constructor(
-    private cart: CartService
-  ) { }
-
-  ngOnInit() {
+  constructor(private cart: CartService, private formBuilder: FormBuilder) {
     this.items = this.cart.getCart();
     this.total = this.cart.getTotalPrice();
+
+    this.checkoutForm = this.formBuilder.group({
+      name: "",
+      address: ""
+    });
   }
 
+  ngOnInit() {}
+
+  onSubmit(customerData) {
+    //Process the order
+
+    this.items = this.cart.clearCart();
+    this.checkoutForm.reset();
+  }
 }
